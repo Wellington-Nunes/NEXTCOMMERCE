2a852a3a201d5041dce445d4450ed41f41ee1cea
Wellington Mariano Nunes <wellingtonnunez10@gmail.com>
2025-03-02 13:28:47 -0300

    feat: implementa consulta à API e tratamento de erros
    - Adiciona consulta à API para alimentar páginas de produtos e detalhes do produto
    - Cria serviço de API centralizado para organizar requisições
    - Implementa tratamento de erros nas páginas de produtos e detalhes

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,18 +1,23 @@
-interface ProductPageProps {
-    params: {
-        id: string;
-    };
-}
+import { ProductPageProps } from "@/types/props";
+import { fetchProduct } from "@/services/productService";
+import Link from "next/link";
 
-const ProductPage = ({ params }: ProductPageProps) => {
-    const { id } = params;
+const ProductPage = async ({ params }: ProductPageProps) => {
+    if (!params?.id) {
+        throw new Error("ID do produto não encontrado");
+      }
+
+    const product = await fetchProduct(params.id);
+
     return (
-        <div
-        >
-            <p>Página de detalhes do produto: {id}</p>
-        </div >
-
+        <div>
+            <Link href={"/products"}>Voltar</Link>
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+            <p>Preço: ${product.price}</p>
+            <p>Categoria: {product.category}</p>
+        </div>
     );
-}
+};
 
 export default ProductPage;
---
+++ src/types/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+  description: string;
+  brand: string;
+  model: string;
+  color: string;
+  category: string;
+  discount: number;
+}
